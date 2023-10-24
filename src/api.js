08d9891ed52a3466957ff241b2fd3fb13baed2f3
Nuhaj Aziz <nuhaj243@gmail.com>
2023-10-24 23:20:45 +0100

    api change

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const shortenLink = async (url) => {
   try {
-    const response = await axios.get(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
-    const { result } = response.data;
-    return result.full_short_link || ''
+    const response = await axios.get(`http://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    const shortLink = response.data;
+    return shortLink || '';
   } catch (error) {
     console.log(error);
     throw new Error('Link shortening failed.');
