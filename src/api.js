e1381de9f70ff265cc1a52b21edf990b5da12483
Nuhaj Aziz <nuhaj243@gmail.com>
2024-04-18 13:12:03 +0100

    api change

--- src/api.js
+++ src/api.js
@@ -2,6 +2,24 @@ import axios from 'axios'
 
 const shortenLink = async (url) => {
   try {
+    const response = await axios.post('https://cleanuri.com/api/v1/shorten', {
+      url: encodeURIComponent(url),
+    });
+    const shortLink = response.data.result_url;
+    return shortLink || '';
+  } catch (error) {
+    console.log(error);
+    throw new Error('Link shortening failed.');
+  }
+};
+
+export default shortenLink;
+
+/*
+import axios from 'axios'
+
+const shortenLink = async (url) => {
+  try {
     const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
     const shortLink = response.data;
     return shortLink || '';
@@ -12,3 +30,4 @@ const shortenLink = async (url) => {
 };
 
 export default shortenLink;
+*/
